060958679f5178d16c56f1b51dc266ba734c07cb
You1538 <137020294+You1538@users.noreply.github.com>
2023-10-25 10:24:30 +0900

    Update app.js

--- app.js
+++ app.js
@@ -48,12 +48,11 @@ arObjectInfo.forEach(objectInfo => {
 
     // データを表示するHTML要素に追加
     const dataElement = document.createElement("div");
-    dataElement.textContent = `
+    dataElement.innerHTML = `
         <a-entity gltf-model="${objectInfo.model}" rotation="${objectInfo.rotation}" scale="${objectInfo.scale}" animation="${objectInfo.animation}" gps-entity-place="latitude:${objectInfo.latitude};longitude:${objectInfo.longitude}">
         </a-entity>`;
     arContainer.appendChild(dataElement);
-    
+
     // ARコンテナに3Dオブジェクトを追加
     arContainer.appendChild(arObject);
-    
 });
