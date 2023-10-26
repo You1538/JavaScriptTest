ccb071fe56214b122770a84b5d650dbe84c58ebb
You1538 <137020294+You1538@users.noreply.github.com>
2023-10-26 14:39:18 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,50 +1,53 @@
-// 3Dオブジェクトの情報をJavaScriptで保管
-const arObjectInfo = [
-    {
-        name: "新発田城",
-        category: "観光",
-        model: "./assets/shibatajo.glb",
-        rotation: "0 0 0",
-        scale: "1 1 1",
-        animation: "",
-        latitude: 37.95493534310519,
-        longitude: 139.32597092399263
-    },
-    // 他のポイントの情報も同様に追加
-    {
-        name: "職短芝生",
-        category: "観光",
-        model: "./assets/sakura.glb",
-        rotation: "0 0 0",
-        scale: "1 1 1",
-        animation: "",
-        latitude: 37.95623964531757,
-        longitude: 139.3371717973987
-    },
+// <a-entity> 要素を生成する関数
+function createAEntity(template, data) {
+    // 新しい <a-entity> 要素を作成
+    const aEntity = document.createElement("a-entity");
 
+    // データを使用して属性を設定
+    for (const key in data) {
+        if (data.hasOwnProperty(key)) {
+            const value = data[key];
+            aEntity.setAttribute(key, value);
+        }
+    }
+
+    // <a-entity> の中身を空に設定 (内部のテキストは空にする)
+    aEntity.textContent = "";
+
+    return aEntity;
+}
+
+// <a-entity> 要素のデータの例
+const entityData = [
     {
-        name: "職短教室",
-        category: "観光",
-        model: "./assets/kingyodaiwa.glb",
-        rotation: "0 0 0",
-        scale: "1 1 1",
-        animation: "",
-        latitude: 37.956133508683955,
-        longitude: 139.3378179331434
+        "gltf-model": "assets/shibatajo.gltf",
+        "rotation": "0 0 0",
+        "scale": "1 1 1",
+        "animation": "property: rotation; to: 0 360 0; loop: true; dur: 2000",
+        "gps-entity-place": "latitude: 37.95493534310519; longitude: 139.32597092399263"
+    },
+    {
+        "gltf-model": "assets/sakura.gltf",
+        "rotation": "0 90 0",
+        "scale": "0.5 0.5 0.5",
+        "animation": "property: rotation; to: 0 360 0; loop: true; dur: 2000",
+        "gps-entity-place": "latitude: 37.95623964531757; longitude: 139.3371717973987"
+    },
+    // 必要に応じてデータを追加
+    {
+        "gltf-model": "assets/sakura.gltf",
+        "rotation": "0 90 0",
+        "scale": "0.5 0.5 0.5",
+        "animation": "",
+        "gps-entity-place": "latitude: 37.956133508683955; longitude: 139.3378179331434"
     },
 ];
 
-// ARコンテナの取得
-const arContainer = document.getElementById("ar-container");
+const result = document.getElementById("result");
 
-// 3Dオブジェクト情報をHTML要素に渡す
-arObjectInfo.forEach(objectInfo => {
-    // データを表示するHTML要素に追加
-    const dataElement = document.createElement("div");
-    // アニメーションが空でない場合にのみ、animation属性を含める
-    const animationAttribute = objectInfo.animation ? `animation="${objectInfo.animation}"` : '';
-    dataElement.innerHTML = `
-        <a-entity gltf-model="${objectInfo.model}" rotation="${objectInfo.rotation}" scale="${objectInfo.scale}" ${animationAttribute} gps-entity-place="latitude:${objectInfo.latitude};longitude:${objectInfo.longitude}">
-        </a-entity>`;
-    arContainer.appendChild(dataElement);
+// <a-entity> 要素を生成して結果に追加
+entityData.forEach(data => {
+    const aEntity = createAEntity('<a-entity></a-entity>', data);
+    result.appendChild(aEntity);
 });
+</script>
