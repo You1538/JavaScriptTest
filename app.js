2e54c2128514360f8ad37c8986673b1632b73d19
You1538 <137020294+You1538@users.noreply.github.com>
2023-11-09 13:25:18 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,52 +1,37 @@
-// <a-entity> 要素を生成する関数
-function createAEntity(template, data) {
-    // 新しい <a-entity> 要素を作成
-    const aEntity = document.createElement("a-entity");
+// app.js
 
-    // データを使用して属性を設定
-    for (const key in data) {
-        if (data.hasOwnProperty(key)) {
-            const value = data[key];
-            aEntity.setAttribute(key, value);
-        }
-    }
-
-    // <a-entity> の中身を空に設定 (内部のテキストは空にする)
-    aEntity.textContent = "";
-
-    return aEntity;
-}
-
-// <a-entity> 要素のデータの例
-const entityData = [
+// 生成する<a-entity>タグの情報をリストで定義
+var entitiesToCreate = [
     {
-        "gltf-model": "assets/shibatajo.glb",
-        "rotation": "0 0 0",
-        "scale": "1 1 1",
-        "animation": "property: rotation; to: 0 360 0; loop: true; dur: 2000",
-        "gps-entity-place": "latitude: 37.95493534310519; longitude: 139.32597092399263"
+      gltfModel: "./assets2/sakura.glb",
+      rotation: "0 0 0",
+      scale: "2 2 2",
+      animation: "",
+      latitude: "37.95623964531757",
+      longitude: "139.3371717973987"
     },
     {
-        "gltf-model": "assets/sakura.glb",
-        "rotation": "0 90 0",
-        "scale": "0.5 0.5 0.5",
-        "animation": "property: rotation; to: 0 360 0; loop: true; dur: 2000",
-        "gps-entity-place": "latitude: 37.95623964531757; longitude: 139.3371717973987"
+      gltfModel: "./assets2/sakura.glb",
+      rotation: "180 180 180",
+      scale: "2 2 2",
+      animation: "",
+      latitude: "37.95623964531757",
+      longitude: "140.123456789"
     },
-    // 必要に応じてデータを追加
-    {
-        "gltf-model": "assets/kingyodaiwa.glb",
-        "rotation": "0 90 0",
-        "scale": "0.5 0.5 0.5",
-        "animation": "",
-        "gps-entity-place": "latitude: 37.956133508683955; longitude: 139.3378179331434"
-    },
-];
-
-const result = document.getElementById("result");
-
-// <a-entity> 要素を生成して結果に追加
-entityData.forEach(data => {
-    const aEntity = createAEntity('<a-entity></a-entity>', data);
-    result.appendChild(aEntity);
-});
+    // 他の<a-entity>情報も同様に追加
+  ];
+  
+  // id="result"の<div>要素を取得
+  var resultContainer = document.getElementById("result");
+  
+  // 各<a-entity>を生成して<div>に追加
+  entitiesToCreate.forEach(function(entityInfo) {
+    var newEntity = document.createElement("a-entity");
+    newEntity.setAttribute("gltf-model", entityInfo.gltfModel);
+    newEntity.setAttribute("rotation", entityInfo.rotation);
+    newEntity.setAttribute("scale", entityInfo.scale);
+    newEntity.setAttribute("animation", entityInfo.animation);
+    newEntity.setAttribute("gps-entity-place", `latitude: ${entityInfo.latitude}; longitude: ${entityInfo.longitude}`);
+    resultContainer.appendChild(newEntity);
+  });
+  
