70fe1bde95243f3a6c57affa76bfbebd3291b588
You1538 <137020294+You1538@users.noreply.github.com>
2023-11-13 09:44:36 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,37 +1,34 @@
+// スクリプトで<a-entity>要素を生成
 var entitiesToCreate = [
-    {
-        gltfModel: "./assets/sakura.glb",
-        rotation: "0 0 0",
-        scale: "2 2 2",
-        animation: "",
-        latitude: "37.95623964531757",
-        longitude: "139.3371717973987"
-    },
-    {
-        gltfModel: "./assets/sakura.glb",
-        rotation: "0 0 180",
-        scale: "2 2 2",
-        animation: "property: rotation; to: 0 0 540; loop: true; dur: 15000; easing: linear;",
-        latitude: "37.95623964531757",
-        longitude: "140.123456789"
-    }
-    // 他の<a-entity>情報も同様に追加
+  {
+    gltfModel: "./assets2/sakura.glb",
+    rotation: "0 0 0",
+    scale: "2 2 2",
+    animation: "",
+    latitude: "37.95623964531757",
+    longitude: "139.3371717973987"
+  },
+  {
+    gltfModel: "./assets2/sakura.glb",
+    rotation: "180 180 180",
+    scale: "2 2 2",
+    animation: "property: rotation; to: 0 360 0; loop: true; dur: 15000; easing: linear;",
+    latitude: "37.95623964531757",
+    longitude: "140.123456789"
+  },
+  // 他の<a-entity>情報も同様に追加
 ];
 
 var scene = document.querySelector("a-scene");
 
-entitiesToCreate.forEach(function (entityInfo) {
-    var newEntity = document.createElement("a-entity");
-    newEntity.setAttribute("gltf-model", entityInfo.gltfModel);
-    newEntity.setAttribute("rotation", entityInfo.rotation);
-    newEntity.setAttribute("scale", entityInfo.scale);
-    newEntity.setAttribute("gps-entity-place", `latitude: ${entityInfo.latitude}; longitude: ${entityInfo.longitude}`);
-
-    if (entityInfo.animation !== "") {
-        var animation = document.createElement("a-animation");
-        animation.setAttribute("attribute", entityInfo.animation);
-        newEntity.appendChild(animation);
-    }
-
-    scene.appendChild(newEntity);
+entitiesToCreate.forEach(function(entityInfo) {
+  var newEntity = document.createElement("a-entity");
+  newEntity.setAttribute("gltf-model", entityInfo.gltfModel);
+  newEntity.setAttribute("rotation", entityInfo.rotation);
+  newEntity.setAttribute("scale", entityInfo.scale);
+  if (entityInfo.animation !== "") {
+      newEntity.setAttribute("animation", entityInfo.animation);
+  }
+  newEntity.setAttribute("gps-entity-place", `latitude: ${entityInfo.latitude}; longitude: ${entityInfo.longitude}`);
+  scene.appendChild(newEntity);
 });
