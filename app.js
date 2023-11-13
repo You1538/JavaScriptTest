272601c61ff69a9de44023ac3ce73faf04f0aba6
You1538 <137020294+You1538@users.noreply.github.com>
2023-11-13 11:35:55 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.getElementById('scene').insertAdjacentHTML('beforeend', `
-<!--0 職短 -->
+        <!--0 職短 -->
         <a-entity gltf-model="./assets/sakura.glb" rotation="0 0 0" scale="2 2 2"
             animation="property: rotation; to: 0 360 0; loop: true; dur: 15000; easing: linear;"
             gps-entity-place="latitude: 37.95623964531757; longitude: 139.3371717973987">
